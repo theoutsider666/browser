7e11806eb4f0e8190166629cd32c71fa0564e30d
theoutsider666 <134681316+theoutsider666@users.noreply.github.com>
2024-11-09 12:02:45 +0800

    Delete src directory

--- src/main/resources/run12.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /usr/local/mysoftware/GSRfinder
-python pre12.py --input $1 --gsr $2 --organism $3 --outdir $4
-
-exit
--- src/main/resources/test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo "hello world"
-call activate %1
-cd %2
-echo %3 %4 %5 %6 %7 %8 %9
-%3 %4 %5 %6 %7 %8 %9
-exit
